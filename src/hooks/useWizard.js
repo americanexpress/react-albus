4f47034b2ac382e956a13186dd6b86c0d4f30a75
Ryan Hinchey <rlhinchey@gmail.com>
2021-03-10 17:01:38 -0500

    feat(api)!: removing withWizard in favor of useWizard
    BREAKING CHANGE: withWizard is no longer available.  Its use case is replaced with useWizard

--- src/hooks/useWizard.js
+++ src/hooks/useWizard.js
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import WizardContext from "../components/Wizard";
+import WizardContext from "../wizardContext";
 
 function useWizard() {
   return useContext(WizardContext);
--- src/index.js
+++ src/index.js
@@ -12,14 +12,7 @@
  * the License.
  */
 
-import createWizardComponent from "./components/createWizardComponent";
-
-const Step = createWizardComponent("Step");
-const WithWizard = createWizardComponent("WithWizard");
-
-export { Step, WithWizard };
-export { WizardProvider as Wizard } from "./components/Wizard";
+export Wizard from "./components/Wizard";
+export Step from "./components/Step";
 export Steps from "./components/Steps";
 export useWizard from "./hooks/useWizard";
-export withWizard from "./withWizard";
-export wizardShape from "./wizardShape";
